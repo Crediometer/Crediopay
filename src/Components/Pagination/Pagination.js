d63962fd4105bc2800c9b54f2df64e03cd14616f
Precious1324 <aleriwaprecious70@gmail.com>
2023-05-30 06:51:19 -0700

    updated

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,18 +1,18 @@
-import { makeStyles } from '@material-ui/core/styles';
+// import { withStyles } from '@mui/material/styles';
 import Pagination from '@mui/material/Pagination';
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& .MuiPagination-ul .Mui-selected': {
-        backgroundColor: '#B11226',
-        color: "white", // Change to your desired color
-      },
-      '& .MuiPagination-ul .Mui-selected .MuiButtonBase-root': {
-        color: 'white', // Change to your desired color
-      },
-    },
-  }));
+// const useStyles = withStyles((theme) => ({
+//     root: {
+//       '& .MuiPagination-ul .Mui-selected': {
+//         backgroundColor: '#B11226',
+//         color: "white", // Change to your desired color
+//       },
+//       '& .MuiPagination-ul .Mui-selected .MuiButtonBase-root': {
+//         color: 'white', // Change to your desired color
+//       },
+//     },
+//   }));
 const Paginations = () => {
-    const classes = useStyles();
+    // const classes = useStyles();
     return ( 
         <div className="main-footer">
             <div className="main-footer-left">
@@ -28,7 +28,7 @@ const Paginations = () => {
                 </div>
             </div>
             <div className="main-footer-right">
-                <Pagination count={1}  classes={{ root: classes.root }} />  
+                <Pagination count={1}   />  
             </div>
         </div>
     );
